55dd7fb3652542c3b60f565cb4028648918a707d
Korcp <48702154+Korcp@users.noreply.github.com>
2023-03-27 02:02:35 +0900

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,79 +1,26 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Permissions;
+using System.Security;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
-
-namespace ArrayEx1
+namespace Stack1
 {
     internal class Program
     {
-
-        class Node
+        static void Main(string[] args)
         {
-            internal int data;
-            internal Node next;
-            public Node(int data)
-            {
-                this.data = data;
-                next = null;
-            }
-        }
-        class LinkedList
-        {
-            private Node head;
-
-            public void addHead(int data)
-            {
-                Node newNode = new Node(data);
-                newNode.next = head;
-                head = newNode;
-            }
-            public int getHead()
-            {
-                if (head == null)
-                    return -1;
-                return head.data;
-            }
-            public void deleteHead() {
-                head = head.next;
-            }
-        }
-          class Stack
-            {
-                private LinkedList list = new LinkedList();
-
-                public void Push(int data)
-                {
-                    list.addHead(data);
-                }
-
-            public int pop()
-            {
-                int temp = list.getHead();
-                list.deleteHead();
-                return temp;
-            }
-
-            }
-        
-
-            static void Main(string[] args)
-            {
+            int[] arr = { 1, 2, 3, 4, 5 };
             Stack stack = new Stack();
-            stack.Push(1);
-            stack.Push(2);
-            stack.Push(3);
-            stack.Push(4);
-            stack.Push(5);
-            Console.WriteLine("pop :"+stack.pop());
-            Console.WriteLine("pop :"+stack.pop());
-            Console.WriteLine("pop :" + stack.pop());
-            Console.WriteLine("pop :" + stack.pop());
-            Console.WriteLine("pop :" + stack.pop());
-        }
-        }
+            for(int i=0; i<arr.Length; i++) {
+                stack.Push(arr[i]);
+            }
+            Console.WriteLine(stack);
+            Console.WriteLine("Peek:"+stack.Peek()+"\n");
+            for (int i = 0; i < arr.Length; i++) 
+            { Console.WriteLine("pop :"+stack.Pop()); }
     }
+    }
+}
 
